5895411b962768a7d787e60751edcbbe4d7c34e9
Rocco Langeweg <rocco.langeweg@gmail.com>
2024-09-30 13:19:02 +0200

    docs: added example for using tokensource in the oauth2 stdlib

---
+++ examples/token_source.go
@@ -0,0 +1,25 @@
+package examples
+
+import (
+	"context"
+	"fmt"
+	"github.com/microserv-io/oauth-credentials-server/pkg/oauth2/tokensource"
+	"golang.org/x/oauth2"
+	"net/url"
+)
+
+func TokenSourceExample() {
+
+	serverEndpoint, err := url.Parse("oauth-credentials-server:8080")
+	if err != nil {
+		panic(fmt.Errorf("failed to parse server endpoint: %w", err))
+	}
+
+	tokenSourceFactory := tokensource.NewFactory(tokensource.WithEndpoint(serverEndpoint))
+
+	// Use the token source to get a token
+	httpClient := oauth2.NewClient(context.TODO(), tokenSourceFactory.CreateTokenSource(context.TODO(), "provider", "resource-owner"))
+
+	// Use the http client to make requests
+	_, _ = httpClient.Get("https://api.example.com")
+}
