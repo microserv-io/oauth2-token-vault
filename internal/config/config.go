7df2698a7d0bd31e15fce8d7c453f954e6c0d83a
Rocco Langeweg <rocco.langeweg@gmail.com>
2024-10-02 13:09:40 +0200

    feat: added validation in config (#8)
    * feat: added validation in config
    
    * feat: added mapstructure to allow snake case yaml, added updated config object back into slice
    
    * test: added tests to validate config file and missing options
    
    * test: set correct env variable in test cases

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,26 +3,31 @@ package config
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"os"
 )
 
 type Provider struct {
-	Name         string
-	ClientID     string
-	ClientSecret string
-	RedirectURL  string
-	AuthURL      string
-	TokenURL     string
-	Scopes       []string
+	Name         string   `mapstructure:"name"`
+	ClientID     string   `mapstructure:"client_id"`
+	ClientSecret string   `mapstructure:"client_secret"`
+	RedirectURL  string   `mapstructure:"redirect_url"`
+	AuthURL      string   `mapstructure:"auth_url"`
+	TokenURL     string   `mapstructure:"token_url"`
+	Scopes       []string `mapstructure:"scopes"`
 }
 
 type Config struct {
 	Providers                 []Provider
-	AllowProviderRegistration bool
+	AllowProviderRegistration bool `mapstructure:"allow_provider_registration"`
 }
 
-func NewConfig(cfgPath string) (*Config, error) {
+func NewConfig(cfgPath string, configFileName string) (*Config, error) {
 
-	viper.SetConfigName("config")
+	if configFileName == "" {
+		configFileName = "config"
+	}
+
+	viper.SetConfigName(configFileName)
 	viper.AddConfigPath(cfgPath)
 	viper.AddConfigPath(".")
 
@@ -33,6 +38,39 @@ func NewConfig(cfgPath string) (*Config, error) {
 	var configObject Config
 	if err := viper.Unmarshal(&configObject); err != nil {
 		return nil, err
+	}
+
+	for i, provider := range configObject.Providers {
+		if provider.Name == "" {
+			return nil, fmt.Errorf("provider name is required for provider at index %d", i)
+		}
+		if provider.ClientID == "" {
+			return nil, fmt.Errorf("client id is required for provider %s", provider.Name)
+		}
+
+		clientSecret := os.Getenv(fmt.Sprintf("PROVIDER__%d__CLIENT_SECRET", i))
+		if clientSecret != "" {
+			provider.ClientSecret = clientSecret
+		}
+
+		if provider.ClientSecret == "" {
+			return nil, fmt.Errorf("client secret is required for provider %s", provider.Name)
+		}
+
+		if provider.RedirectURL == "" {
+			return nil, fmt.Errorf("redirect url is required for provider %s", provider.Name)
+		}
+		if provider.AuthURL == "" {
+			return nil, fmt.Errorf("auth url is required for provider %s", provider.Name)
+		}
+		if provider.TokenURL == "" {
+			return nil, fmt.Errorf("token url is required for provider %s", provider.Name)
+		}
+		if len(provider.Scopes) == 0 {
+			return nil, fmt.Errorf("scopes are required for provider %s", provider.Name)
+		}
+
+		configObject.Providers[i] = provider
 	}
 
 	return &configObject, nil
---
+++ internal/config/config_test.go
@@ -0,0 +1,171 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestNewConfig(t *testing.T) {
+	wd, _ := os.Getwd()
+	rootDir := filepath.Join(wd, "../..")
+	cfgPath := filepath.Join(rootDir, "tests/testdata/config")
+
+	tests := []struct {
+		name       string
+		cfgFile    string
+		envVars    map[string]string
+		wantErr    bool
+		wantConfig *Config
+	}{
+		{
+			name:    "valid config file",
+			cfgFile: "valid_config",
+			envVars: map[string]string{
+				"PROVIDER__0__CLIENT_SECRET": "env_client_secret",
+			},
+			wantErr: false,
+			wantConfig: &Config{
+				Providers: []Provider{
+					{
+						Name:         "google",
+						ClientID:     "google-client-id",
+						ClientSecret: "env_client_secret",
+						RedirectURL:  "http://localhost:8080/oauth/google/callback",
+						AuthURL:      "https://accounts.google.com/o/oauth2/auth",
+						TokenURL:     "https://accounts.google.com/o/oauth2/token",
+						Scopes:       []string{"https://www.googleapis.com/auth/analytics"},
+					},
+				},
+				AllowProviderRegistration: true,
+			},
+		},
+		{
+			name:       "valid without client secret",
+			cfgFile:    "valid_config",
+			envVars:    map[string]string{},
+			wantErr:    true,
+			wantConfig: nil,
+		},
+		{
+			name:    "missing client id",
+			cfgFile: "missing_client_id",
+			envVars: map[string]string{
+				"PROVIDER__0__CLIENT_SECRET": "env_client_secret",
+			},
+			wantErr:    true,
+			wantConfig: nil,
+		},
+		{
+			name:    "missing provider name",
+			cfgFile: "missing_provider_name",
+			envVars: map[string]string{
+				"PROVIDER__0__CLIENT_SECRET": "env_client_secret",
+			},
+			wantErr:    true,
+			wantConfig: nil,
+		},
+		{
+			name:    "missing redirect url",
+			cfgFile: "missing_redirect_url",
+			envVars: map[string]string{
+				"PROVIDER__0__CLIENT_SECRET": "env_client_secret",
+			},
+			wantErr:    true,
+			wantConfig: nil,
+		},
+		{
+			name:    "missing auth url",
+			cfgFile: "missing_auth_url",
+			envVars: map[string]string{
+				"PROVIDER__0__CLIENT_SECRET": "env_client_secret",
+			},
+			wantErr:    true,
+			wantConfig: nil,
+		},
+		{
+			name:    "missing token url",
+			cfgFile: "missing_token_url",
+			envVars: map[string]string{
+				"PROVIDER__0__CLIENT_SECRET": "env_client_secret",
+			},
+			wantErr:    true,
+			wantConfig: nil,
+		},
+		{
+			name:    "missing scopes",
+			cfgFile: "missing_scopes",
+			envVars: map[string]string{
+				"PROVIDER__0__CLIENT_SECRET": "env_client_secret",
+			},
+			wantErr:    true,
+			wantConfig: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Set environment variables
+			for key, value := range tt.envVars {
+				_ = os.Setenv(key, value)
+				defer func(key string) {
+					_ = os.Unsetenv(key)
+				}(key)
+			}
+
+			gotConfig, err := NewConfig(cfgPath, tt.cfgFile)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr && !compareConfigs(gotConfig, tt.wantConfig) {
+				t.Errorf("NewConfig() = %v, want %v", gotConfig, tt.wantConfig)
+			}
+		})
+	}
+}
+
+func (p Provider) NotEqual(other Provider) bool {
+	if p.Name != other.Name {
+		return true
+	}
+	if p.ClientID != other.ClientID {
+		return true
+	}
+	if p.ClientSecret != other.ClientSecret {
+		return true
+	}
+	if p.RedirectURL != other.RedirectURL {
+		return true
+	}
+	if p.AuthURL != other.AuthURL {
+		return true
+	}
+	if p.TokenURL != other.TokenURL {
+		return true
+	}
+	if len(p.Scopes) != len(other.Scopes) {
+		return true
+	}
+	for i := range p.Scopes {
+		if p.Scopes[i] != other.Scopes[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func compareConfigs(a, b *Config) bool {
+	if a.AllowProviderRegistration != b.AllowProviderRegistration {
+		return false
+	}
+	if len(a.Providers) != len(b.Providers) {
+		return false
+	}
+	for i := range a.Providers {
+		if a.Providers[i].NotEqual(b.Providers[i]) {
+			return false
+		}
+	}
+	return true
+}
