c02abe80ee801c7dbf53c3495de8ec0fe6cad477
Rocco Langeweg <rocco.langeweg@gmail.com>
2024-10-09 21:27:59 +0200

    test: added tests for remaining provider rpc endpoints

--- internal/infrastructure/grpc/providerservice/v1/grpc_test.go
+++ internal/infrastructure/grpc/providerservice/v1/grpc_test.go
@@ -68,3 +68,165 @@ func TestService_ListProviders(t *testing.T) {
 		})
 	}
 }
+
+func TestService_CreateProvider(t *testing.T) {
+	tests := []struct {
+		name          string
+		mockSetup     func(mockProviderService *MockProviderService)
+		expectedError error
+		expectedResp  *oauthcredentials.CreateProviderResponse
+	}{
+		{
+			name: "success",
+			mockSetup: func(mockProviderService *MockProviderService) {
+				mockProviderService.EXPECT().CreateProvider(mock.Anything, &provider.CreateInput{
+					Name:    "provider1",
+					AuthURL: "http://auth.url",
+				}, "api").Return(&provider.CreateProviderResponse{
+					Provider: &provider.Provider{
+						Name: "provider1", AuthURL: "http://auth.url", TokenURL: "http://token.url", Scopes: []string{"scope1"}, ClientID: "client1",
+					},
+				}, nil)
+			},
+			expectedError: nil,
+			expectedResp: &oauthcredentials.CreateProviderResponse{
+				OauthProvider: &oauthcredentials.OAuthProvider{
+					Name: "provider1", AuthUrl: "http://auth.url", TokenUrl: "http://token.url", Scopes: []string{"scope1"}, ClientId: "client1",
+				},
+			},
+		},
+		{
+			name: "error",
+			mockSetup: func(mockProviderService *MockProviderService) {
+				mockProviderService.EXPECT().CreateProvider(mock.Anything, &provider.CreateInput{
+					Name:    "provider1",
+					AuthURL: "http://auth.url",
+				}, "api").Return(nil, errors.New("some error"))
+			},
+			expectedError: errors.New("failed to create provider: some error"),
+			expectedResp:  nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockProviderService := NewMockProviderService(t)
+			tt.mockSetup(mockProviderService)
+
+			s := NewService(mockProviderService)
+
+			resp, err := s.CreateProvider(context.TODO(), &oauthcredentials.CreateProviderRequest{
+				Name:    "provider1",
+				AuthUrl: "http://auth.url",
+			})
+
+			if tt.expectedError != nil {
+				assert.EqualError(t, err, tt.expectedError.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expectedResp, resp)
+			}
+		})
+	}
+}
+
+func TestService_UpdateProvider(t *testing.T) {
+	tests := []struct {
+		name          string
+		mockSetup     func(mockProviderService *MockProviderService)
+		expectedError error
+		expectedResp  *oauthcredentials.UpdateProviderResponse
+	}{
+		{
+			name: "success",
+			mockSetup: func(mockProviderService *MockProviderService) {
+				mockProviderService.EXPECT().UpdateProvider(mock.Anything, "provider1", &provider.UpdateInput{
+					AuthURL: "http://auth.url",
+				}).Return(&provider.UpdateProviderResponse{
+					Provider: &provider.Provider{
+						Name: "provider1", AuthURL: "http://auth.url", TokenURL: "http://token.url", Scopes: []string{"scope1"}, ClientID: "client1",
+					},
+				}, nil)
+			},
+			expectedError: nil,
+			expectedResp: &oauthcredentials.UpdateProviderResponse{
+				OauthProvider: &oauthcredentials.OAuthProvider{
+					Name: "provider1", AuthUrl: "http://auth.url", TokenUrl: "http://token.url", Scopes: []string{"scope1"}, ClientId: "client1",
+				},
+			},
+		},
+		{
+			name: "error",
+			mockSetup: func(mockProviderService *MockProviderService) {
+				mockProviderService.EXPECT().UpdateProvider(mock.Anything, "provider1", &provider.UpdateInput{
+					AuthURL: "http://auth.url",
+				}).Return(nil, errors.New("some error"))
+			},
+			expectedError: errors.New("failed to update provider: some error"),
+			expectedResp:  nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockProviderService := NewMockProviderService(t)
+			tt.mockSetup(mockProviderService)
+
+			s := NewService(mockProviderService)
+
+			resp, err := s.UpdateProvider(context.TODO(), &oauthcredentials.UpdateProviderRequest{
+				Name:    "provider1",
+				AuthUrl: "http://auth.url",
+			})
+
+			if tt.expectedError != nil {
+				assert.EqualError(t, err, tt.expectedError.Error())
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expectedResp, resp)
+			}
+		})
+	}
+}
+
+func TestService_DeleteProvider(t *testing.T) {
+	tests := []struct {
+		name          string
+		mockSetup     func(mockProviderService *MockProviderService)
+		expectedError error
+	}{
+		{
+			name: "success",
+			mockSetup: func(mockProviderService *MockProviderService) {
+				mockProviderService.EXPECT().DeleteProvider(mock.Anything, "provider1").Return(nil)
+			},
+			expectedError: nil,
+		},
+		{
+			name: "error",
+			mockSetup: func(mockProviderService *MockProviderService) {
+				mockProviderService.EXPECT().DeleteProvider(mock.Anything, "provider1").Return(errors.New("some error"))
+			},
+			expectedError: errors.New("failed to delete provider: some error"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockProviderService := NewMockProviderService(t)
+			tt.mockSetup(mockProviderService)
+
+			s := NewService(mockProviderService)
+
+			_, err := s.DeleteProvider(context.TODO(), &oauthcredentials.DeleteProviderRequest{
+				Id: "provider1",
+			})
+
+			if tt.expectedError != nil {
+				assert.EqualError(t, err, tt.expectedError.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
