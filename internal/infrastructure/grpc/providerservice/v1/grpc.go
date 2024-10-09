6e781781b77291ef45a1602fd97f664b4622596d
Rocco Langeweg <rocco.langeweg@gmail.com>
2024-10-09 14:57:20 +0200

    feat: updated grpc server to add provider service as dependency

--- internal/infrastructure/grpc/providerservice/v1/grpc.go
+++ internal/infrastructure/grpc/providerservice/v1/grpc.go
@@ -1,31 +1,104 @@
-package v1
+package providerservice
 
 import (
 	"context"
+	"fmt"
+	"github.com/microserv-io/oauth-credentials-server/internal/app/oauthapp"
+	"github.com/microserv-io/oauth-credentials-server/internal/app/provider"
 	"github.com/microserv-io/oauth-credentials-server/pkg/proto/oauthcredentials/v1"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 var _ oauthcredentials.OAuthProviderServiceServer = &Service{}
 
 type Service struct {
 	oauthcredentials.UnimplementedOAuthProviderServiceServer
+	providerService *provider.Service
+	oauthappService *oauthapp.Service
 }
 
-func NewService() *Service {
-	return &Service{}
+func NewService(
+	providerService *provider.Service,
+	oauthAppService *oauthapp.Service,
+) *Service {
+	return &Service{
+		providerService: providerService,
+		oauthappService: oauthAppService,
+	}
 }
 
 func (s Service) ListProviders(_ *oauthcredentials.ListProvidersRequest, stream oauthcredentials.OAuthProviderService_ListProvidersServer) error {
-	return status.Errorf(codes.Unimplemented, "method ListProviders not implemented")
+	resp, err := s.providerService.ListProviders(stream.Context())
+	if err != nil {
+		return fmt.Errorf("failed to list providers: %w", err)
+	}
+
+	var oauthProviders []*oauthcredentials.OAuthProvider
+
+	for _, p := range resp.Providers {
+		oauthProviders = append(oauthProviders, &oauthcredentials.OAuthProvider{
+			Name:     p.Name,
+			AuthUrl:  p.AuthURL,
+			TokenUrl: p.TokenURL,
+			Scopes:   p.Scopes,
+			ClientId: p.ClientID,
+		})
+	}
+
+	if err := stream.Send(&oauthcredentials.ListProvidersResponse{OauthProviders: oauthProviders}); err != nil {
+		return fmt.Errorf("failed to send provider: %w", err)
+	}
+
+	return nil
 }
+
 func (s Service) CreateProvider(ctx context.Context, oauthProvider *oauthcredentials.CreateProviderRequest) (*oauthcredentials.CreateProviderResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateProvider not implemented")
+	resp, err := s.providerService.CreateProvider(ctx, &provider.CreateInput{
+		Name:    oauthProvider.Name,
+		AuthURL: oauthProvider.AuthUrl,
+	}, "api")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create provider: %w", err)
+	}
+
+	return &oauthcredentials.CreateProviderResponse{
+		OauthProvider: &oauthcredentials.OAuthProvider{
+			Name:     resp.Provider.Name,
+			AuthUrl:  resp.Provider.AuthURL,
+			TokenUrl: resp.Provider.TokenURL,
+			Scopes:   resp.Provider.Scopes,
+			ClientId: resp.Provider.ClientID,
+		},
+	}, nil
 }
 func (s Service) UpdateProvider(ctx context.Context, oauthProvider *oauthcredentials.UpdateProviderRequest) (*oauthcredentials.UpdateProviderResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateProvider not implemented")
+	resp, err := s.providerService.Update(ctx, oauthProvider.Name, &provider.UpdateInput{
+		ClientID:     oauthProvider.ClientId,
+		ClientSecret: oauthProvider.ClientSecret,
+		RedirectURI:  oauthProvider.RedirectUri,
+		Scopes:       oauthProvider.Scopes,
+		AuthURL:      oauthProvider.AuthUrl,
+		TokenURL:     oauthProvider.TokenUrl,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to update provider: %w", err)
+	}
+
+	return &oauthcredentials.UpdateProviderResponse{
+		OauthProvider: &oauthcredentials.OAuthProvider{
+			Name:     resp.Provider.Name,
+			AuthUrl:  resp.Provider.AuthURL,
+			TokenUrl: resp.Provider.TokenURL,
+			Scopes:   resp.Provider.Scopes,
+			ClientId: resp.Provider.ClientID,
+		},
+	}, nil
 }
+
 func (s Service) DeleteProvider(ctx context.Context, oauthProvider *oauthcredentials.DeleteProviderRequest) (*oauthcredentials.DeleteProviderResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method DeleteProvider not implemented")
+	err := s.providerService.DeleteProvider(ctx, oauthProvider.GetId())
+	if err != nil {
+		return nil, fmt.Errorf("failed to delete provider: %w", err)
+	}
+
+	return &oauthcredentials.DeleteProviderResponse{}, nil
 }
